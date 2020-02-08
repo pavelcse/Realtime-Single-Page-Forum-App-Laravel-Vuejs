ec1a9262d7988747d18fed500c1023ce7672bb2f
Pavel Parvez <pavel.cse26@gmail.com>
2020-02-08 10:12:37 +0600

    Like Dislike for Reply

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,12 @@
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-
+/** Route */
 Route::apiResource('/questions', 'QuestionController');
+
 Route::apiResource('/categories', 'CategoryController');
+
 Route::apiResource('/questions/{question}/replies', 'ReplyController');
+
+Route::post('like/{reply}', 'LikeController@likeIt');
+Route::delete('like/{reply}', 'LikeController@unLikeIt');
